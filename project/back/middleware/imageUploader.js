ddf1157fd8aff42574aa84b6f291f5bc0ac311df
서지영 Jiyeong Seo <119486106+seoziyeong@users.noreply.github.com>
2023-07-27 15:13:42 +0900

    Update imageUploader.js

--- project/back/middleware/imageUploader.js
+++ project/back/middleware/imageUploader.js
@@ -5,12 +5,10 @@ const path = require("path");
 
 AWS.config.update({
   region: "ap-northeast-2", // AWS의 region 값
-  accessKeyId: "AKIASA22XFKTC5X3HJ5X",
-  secretAccessKey: "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",
 
   // 테스트 후 배포할 때는 보안을 위해 env 파일로 변경해주기.
-  // accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-  // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
 });
 
 const s3 = new AWS.S3();
